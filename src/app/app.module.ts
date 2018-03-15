d735cd2c34a31c1688d72ca79e5c6dec7192ac2f
elexfreeman <elextraza@gmail.com>
2018-03-15 12:25:58 +0400

    добавил bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
+import { NavAdminComponent } from './nav-admin/nav-admin.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavAdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
+  ,
+  exports: [BsDropdownModule, TooltipModule, ModalModule]
 })
 export class AppModule { }
